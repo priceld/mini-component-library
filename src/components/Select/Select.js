60eb6aa16993536d174258d7e23441af09da37fa
Logan Price <logan.price@blackboard.com>
2021-08-11 09:24:32 -0400

    feat: Add styles for Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,60 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <InvisibleNativeSelect value={value} onChange={onChange}>
+        {children}
+      </InvisibleNativeSelect>
+      <VisibleSelect>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" size={24} strokeWidth={1} />
+        </IconWrapper>
+      </VisibleSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const InvisibleNativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const VisibleSelect = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  padding-right: 52px;
+  border: none;
+  border-radius: 8px;
+
+  ${InvisibleNativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+  ${InvisibleNativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`;
 
 export default Select;
